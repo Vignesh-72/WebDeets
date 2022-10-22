08b5e4b6d730e5de7467267973021a2c1c5acb2a
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-10-22 20:49:17 +0530

    Update terminal.java

--- src/terminal.java
+++ src/terminal.java
@@ -58,13 +58,14 @@ public class terminal{
     public void ping_Section(int Option , String Link , String nameOfTheFile) throws IOException{
         
         ArrayList<String> subCommand = new ArrayList<>();
-        subCommand.add(" 4");
-        subCommand.add(" 4");
-        subCommand.add(" 32");
-        subCommand.add(" 4");
-        subCommand.add(" 4");
-        subCommand.add(" 10");
-        subCommand.add(" ");
+        
+        subCommand.add("");
+        subCommand.add("4");
+        subCommand.add("32");
+        subCommand.add("4");
+        subCommand.add("4");
+        subCommand.add("10");
+        subCommand.add("");
          
         ArrayList<String> deetsCommand = new ArrayList<>();
         this.target = Link;
@@ -76,49 +77,46 @@ public class terminal{
         deetsCommand.add("ping -i ");
         deetsCommand.add("ping -p ");
 
-    
-        /*if(Option == 8){
+        if(Option == 8){
             String temp;
-
-            System.out.println("$ Enter Times To Ping Limit [1] To [4294967295]");
+            
+            System.out.print("$ Enter Times To Ping Limit [1] To [4294967295] : ");
             temp = scanner.next();
-            subCommand.set(1, temp);
+            subCommand.add(1, temp);
         
-            System.out.print("$ Enter In Bytes, Of The Echo-Request Packet : \n");
+            System.out.print("\n$ Enter In Bytes, Of The Echo-Request Packet : ");
             temp = scanner.next();
             subCommand.set(2, temp);
         
-            System.out.println("$ The maximum count value is 4, i.e. only the first four hops can be time stamped");
-            System.out.print("$ ");
+            System.out.print("\n$ The maximum count value is 4, i.e. only the first four hops can be time stamped");
+            System.out.print("\n$ : ");
             temp = scanner.next();
             subCommand.set(3, temp);
       
-            System.out.print("$ Numbers Of Hops Max Value [9] : ");
+            System.out.print("\n$ Numbers Of Hops Max Value [9] : ");
             temp = scanner.next();
             subCommand.set(4, temp);
 
-            System.out.print("$ Set Time To Live [TTL] Max Value [255] : ");
+            System.out.print("\n$ Set Time To Live [TTL] Max Value [255] : ");
             temp = scanner.next();
             subCommand.set(5, temp);
-            for (String string : deetsCommand) {
-                for (String string2 : subCommand) {
-                    System.out.println(string + " " + string2);
-                }
+
+            for (int i = 0; i < 6; i++){
+            this.Command = deetsCommand.get(i) + subCommand.get(i) + " " +target;
+            System.out.println(Command);
+            process();
+
             }
+            
         }else{
-            this.Command = deetsCommand.get(Option) + subCommand.get(Option) + " " + target;
+            this.Command = deetsCommand.get(Option) + subCommand.get(Option) + target;
             process();
         }
-       
-       */
-        System.out.println(Option);
-        System.out.println(deetsCommand.get(Option));
-        System.out.println(deetsCommand.get(Option) + subCommand.get(Option) + " " + target);
+
         if(nameOfTheFile != "null"){
             this.Path = nameOfTheFile;
             createFile();}
     }
-    
     public void pc_Info_Section(int option , String nameOfTheFile) throws IOException{
         ArrayList<String> netStatCommand = new ArrayList<>();
         netStatCommand.add("netstat -a");
@@ -146,5 +144,5 @@ public class terminal{
     }
         
     }
-
+  
 
