60813ec72c829a6b62198b1fd4f00872faffc381
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-11-13 19:12:57 +0530

    Delete webDeetsApp/docs directory

--- webDeetsApp/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"webPack","c":"deetsMain","l":"deetsMain()","u":"%3Cinit%3E()"},{"p":"webPack","c":"deetsMain","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
