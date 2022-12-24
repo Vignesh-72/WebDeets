b047bbde5e7749cadb7b1c1cc1cbdfd62ca6a395
Vicky <107568580+HungryVicky@users.noreply.github.com>
2022-12-24 11:35:54 +0530

    Add files via upload

---
+++ build.cmd
@@ -0,0 +1,30 @@
+ECHO "Build Started"
+CD src
+
+ECHO "Building Docs"
+javadoc Application.java -d ../docs 
+javadoc WDFrame/*.java -d ../docs
+javadoc WDCore/Core.java -d ../docs
+ECHO "Build Docs Successful"
+
+ECHO "Building Class"
+javac Application.java -d ../bin/WDClass/ 
+ECHO "Build Class Successful"
+
+ECHO "Excutiong Application"
+CD ../ && CD bin/WDClass/
+java Application
+
+ECHO "Press Enter To Create Jar"
+PAUSE
+
+ECHO "Building Jar"
+CD ../
+jar cvfm Application.jar resources/manifest.txt -C bin . src docs
+
+
+
+
+
+
+
